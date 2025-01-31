03daf3c735ec81976999d07f9afd0fc562f7287f
Nathan Traxler <nathantraxler@MacBook-Pro.local>
2025-01-31 07:42:40 -0800

    Add basic router within orchestration service.

---
+++ apps/server/src/services/llmService.ts
@@ -0,0 +1,74 @@
+import OpenAI from 'openai';
+import openai from '../setup/openai';
+import { Errors, logAndThrowError } from '../utils/errors';
+
+export async function invokeCompletionAPI(
+    config: OpenAI.Chat.Completions.ChatCompletionCreateParamsNonStreaming
+) {
+    let content: string;
+    try {
+        const completion = await openai.chat.completions.create(config);
+
+        if (!completion.choices[0].message.content) {
+            logAndThrowError({
+                message: 'Completion message does not have content.',
+                code: Errors.LLM_API_ERROR
+            });
+        }
+
+        if (completion.choices[0].message.refusal) {
+            logAndThrowError({
+                message: 'The LLM refused to answer that prompt.',
+                code: Errors.LLM_API_ERROR
+            });
+        }
+
+        content = completion.choices[0].message.content;
+    } catch (error) {
+        logAndThrowError({
+            message: 'An error occurred requesting LLM API.',
+            error,
+            code: Errors.LLM_API_ERROR
+        });
+    }
+    return content;
+}
+
+export async function invokeStructuredCompletionAPI<T>(
+    config: OpenAI.Chat.Completions.ChatCompletionCreateParamsNonStreaming
+): Promise<T> {
+    let structuredResponse: T;
+    try {
+        const completion = await openai.beta.chat.completions.parse(config);
+        if (completion.choices[0].finish_reason === 'length') {
+            // Handle the case where the model did not return a complete response
+            logAndThrowError({
+                message: 'Model did not return a complete response.',
+                code: Errors.LLM_API_ERROR
+            });
+        }
+
+        if (!completion.choices[0].message.parsed) {
+            logAndThrowError({
+                message: 'Structured message returned null.',
+                code: Errors.LLM_API_ERROR
+            });
+        }
+
+        if (completion.choices[0].message.refusal) {
+            logAndThrowError({
+                message: 'The LLM refused to answer that prompt.',
+                code: Errors.LLM_API_ERROR
+            });
+        }
+
+        structuredResponse = completion.choices[0].message.parsed;
+    } catch (error) {
+        logAndThrowError({
+            message: 'An error occurred requesting LLM API.',
+            error,
+            code: Errors.LLM_API_ERROR
+        });
+    }
+    return structuredResponse;
+}
--- apps/server/src/services/orchestrationService.ts
+++ apps/server/src/services/orchestrationService.ts
@@ -1,3 +1,66 @@
-export function respond(prompt: string) {
-    return prompt;
+import { zodResponseFormat } from 'openai/helpers/zod';
+import { z } from 'zod';
+import * as llmService from './llmService';
+
+type Message = { author: string; content: string };
+
+const memory = new Map<number, Message[]>();
+memory.set(1, []);
+
+const ActionEnum = z.enum(['SUGGEST_RECIPES', 'BUILD_SHOPPING_LIST', 'GENERATE_MENU', 'MORE_INFO']);
+
+const RouteResponseFormat = z.object({ action: ActionEnum });
+
+type Action = z.infer<typeof ActionEnum>;
+
+type RouteResponse = z.infer<typeof RouteResponseFormat>;
+
+export async function respond(prompt: string) {
+    const messages = memory.get(1) ?? [];
+    const humanMessage: Message = { author: 'user', content: prompt };
+    memory.set(1, [...messages, humanMessage]);
+    let action = await routeRequest(memory.get(1) ?? []);
+    action = await executeRoute(action);
+    const aiMessage = { author: 'sous chef', content: action };
+    memory.set(1, [...messages, aiMessage]);
+
+    return action;
+}
+
+async function routeRequest(messages: Message[]): Promise<Action> {
+    const routeResponse = await llmService.invokeStructuredCompletionAPI<RouteResponse>({
+        model: process.env.GENERATE_RECIPE_MODEL ?? 'gpt-4o',
+        messages: [
+            {
+                role: 'system',
+                content: `
+                        You are a message router. Based on the conversation history you will route the request to
+                        the appropriate best next action.
+                    `
+            },
+            {
+                role: 'user',
+                content: JSON.stringify(messages)
+            }
+        ],
+        response_format: zodResponseFormat(RouteResponseFormat, 'action')
+    });
+    return routeResponse.action;
+}
+
+async function executeRoute(action: Action): Promise<Action> {
+    switch (action) {
+        case ActionEnum.enum.SUGGEST_RECIPES: {
+            return action;
+        }
+        case ActionEnum.enum.BUILD_SHOPPING_LIST: {
+            return action;
+        }
+        case ActionEnum.enum.GENERATE_MENU: {
+            return action;
+        }
+        case ActionEnum.enum.MORE_INFO: {
+            return action;
+        }
+    }
 }
